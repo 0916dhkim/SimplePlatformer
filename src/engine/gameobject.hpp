5d79e42d0188ca2a549bdfcfcf82a8896b7ccd8a
Donghyeon Kim <donghyeon@tacocube.com>
2018-04-01 22:39:03 -0400

    Add Transform to GameObject

--- src/engine/gameobject.hpp
+++ src/engine/gameobject.hpp
@@ -1,6 +1,13 @@
 #ifndef SIMPLEPLATFORMER_ENGINE_GAMEOBJECT_H
 #define SIMPLEPLATFORMER_ENGINE_GAMEOBJECT_H
 class GameObject {
-  // TODO Implement GameObject
+ public:
+  struct Transform {
+    struct Position {
+      float x = 0;  // x coordinate
+      float y = 0;  // y coordinate
+    } position;
+    float rotation = 0;  // rotation in radians
+  } transform;
 };
 #endif  // SIMPLEPLATFORMER_ENGINE_GAMEOBJECT_H
