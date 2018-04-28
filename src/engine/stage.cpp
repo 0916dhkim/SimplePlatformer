012a8125f8da7fffd3fbbc2fa4dc7010c04e4bf4
Donghyeon Kim <donghyeon@tacocube.com>
2018-04-28 00:54:37 -0400

    Add GameInfo Member in Stage Class

--- src/engine/stage.cpp
+++ src/engine/stage.cpp
@@ -25,3 +25,12 @@ std::pair<std::map<std::uint_fast64_t, std::shared_ptr<Actor>>::iterator,
 Stage::GetActors() {
   return std::make_pair(actors.begin(), actors.end());
 }
+
+std::shared_ptr<GameInfo> Stage::CreateGameInfo() {
+  gameinfo.reset(new GameInfo());
+  return gameinfo;
+}
+
+std::shared_ptr<GameInfo> Stage::GetGameInfo() {
+  return gameinfo;
+}
--- src/engine/stage.hpp
+++ src/engine/stage.hpp
@@ -1,6 +1,7 @@
 #ifndef SIMPLEPLATFORMER_ENGINE_STAGE_H
 #define SIMPLEPLATFORMER_ENGINE_STAGE_H
 #include <engine/actor.hpp>
+#include <engine/gameinfo.hpp>
 #include <map>
 #include <memory>
 class Stage {
@@ -23,9 +24,16 @@ public:
             std::map<std::uint_fast64_t, std::shared_ptr<Actor>>::iterator>
   GetActors();
 
+  // Create new gameinfo object and discard the old one.
+  std::shared_ptr<GameInfo> CreateGameInfo();
+  std::shared_ptr<GameInfo> GetGameInfo();
+
 private:
   // All actors
   std::map<std::uint_fast64_t, std::shared_ptr<Actor>> actors;
+
+  // Game information
+  std::shared_ptr<GameInfo> gameinfo;
 
   // Camera
   Camera camera;
