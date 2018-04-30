163f077d4e95d4b71d3688bf1423defa396332e0
Donghyeon Kim <donghyeon@tacocube.com>
2018-04-30 14:31:29 -0400

    Get Initialization Order Right

--- src/game/hero.cpp
+++ src/game/hero.cpp
@@ -3,7 +3,7 @@
 #include <engine/physics/dynamic_triangle.hpp>
 #include <engine/renderer/triangle_renderer.hpp>
 #include <game/hero.hpp>
-Hero::Hero(std::uint_fast64_t id) : Actor(id), move_speed(10), moving_left(false), moving_right(false) {
+Hero::Hero(std::uint_fast64_t id) : Actor(id), move_speed(10), moving_right(false), moving_left(false) {
   SetRenderer(std::unique_ptr<Renderer>(new TriangleRenderer(*this, 1, b2Vec2(0.5, 0.5), Color::RED)));
   SetBody(std::unique_ptr<PhysicalBody>(new DynamicTriangle(*this, b2Vec2(0.5, 0.5), 1)));
   AddTag("hero");
